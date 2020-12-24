f062c69732290875a7fe5d69a24157f753054c6a
Gabriel <gaplogan@gmail.com>
2020-12-24 16:40:18 -0300

    2020.12.24.16.40

---
+++ AvisoFesta2/src/AvisoFesta2.java
@@ -0,0 +1,14 @@
+public class AvisoFesta2 {
+    public static void main(String[] args) throws Exception {
+        //declara variável inteira para controle do loop
+        int j = 1; //inicializa variável de controle do loop
+            
+        while (j <= 5)
+        {//início do loop
+            //escreve a frase
+            System.out.println("Hoje vai ter festa na casa da Fabi");
+            //incrementa variável de controle do loop (contador)
+            j++; // Mesmo que j = j + 1;
+        }//fim do loop
+    }
+}
